d144173d970a8ec1f5b9575dd925fe1518a2faad
Yen Tran <yen.tran@autodesk.com>
2016-10-21 09:47:51 -0700

    refactors listTodos, addTodo, deleteTodo

--- script.js
+++ script.js
@@ -5,21 +5,33 @@ var todos = [],
 
 while (input !== "quit") {
     if (input === "list") {
-    		console.log("************");
-        todos.forEach(function(todo, i) {
-        	console.log(i + ": " + todo);
-        });
-        console.log("************");
+        listTodos();
     } else if (input === "new") {
-        var newTodo = prompt("Enter a new todo");
-        todos.push(newTodo);
-        console.log("Added Todo");
-    } else if (input === "delete"){
-    		var deleteIndex = prompt("Enter index of todo to delete");
-    				todos.splice(deleteIndex, 1);
-    				console.log("Deleted Todo");
+        addTodo();
+    } else if (input === "delete") {
+        deleteTodo();
     }
     input = prompt("What would you like to do?");
 };
 
 console.log("OK, YOU QUIT THE APP");
+
+function listTodos() {
+    console.log("************");
+    todos.forEach(function(todo, i) {
+        console.log(i + ": " + todo);
+    });
+    console.log("************");
+}
+
+function addTodo() {
+    var newTodo = prompt("Enter a new todo");
+    todos.push(newTodo);
+    console.log("Added Todo");
+}
+
+function deleteTodo() {
+    var deleteIndex = prompt("Enter index of todo to delete");
+    todos.splice(deleteIndex, 1);
+    console.log("Deleted Todo");
+}
